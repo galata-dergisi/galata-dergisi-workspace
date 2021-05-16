763478f1373744ad97ba3d5da08ac1694c1cd4fc
Mehmet Baker <mehmet@mehmetbaker.dev>
2021-05-16 18:54:56 +0300

    Modified copyright & license notice comments
    So they can be folded in VS Code.
    
    Signed-off-by: Mehmet Baker <mehmet@mehmetbaker.dev>

--- database-migrations/1621005516513-page.ts
+++ database-migrations/1621005516513-page.ts
@@ -1,21 +1,23 @@
-// Copyright 2021 Mehmet Baker
-//
-// This file is part of galata-dergisi-workspace.
-//
-// galata-dergisi-workspace is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// galata-dergisi-workspace is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with galata-dergisi-workspace. If not, see <https://www.gnu.org/licenses/>.
+/**
+ * Copyright 2021 Mehmet Baker
+ *
+ * This file is part of galata-dergisi-workspace.
+ *
+ * galata-dergisi-workspace is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * galata-dergisi-workspace is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with galata-dergisi-workspace. If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import { MigrationInterface, QueryRunner, Table, TableForeignKey, TableIndex, TableUnique } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableForeignKey, TableIndex } from 'typeorm';
 
 export class page1621005516513 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
--- database-migrations/1621006535738-contributor.ts
+++ database-migrations/1621006535738-contributor.ts
@@ -1,19 +1,21 @@
-// Copyright 2021 Mehmet Baker
-//
-// This file is part of galata-dergisi-workspace.
-//
-// galata-dergisi-workspace is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// galata-dergisi-workspace is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with galata-dergisi-workspace. If not, see <https://www.gnu.org/licenses/>.
+/**
+ * Copyright 2021 Mehmet Baker
+ *
+ * This file is part of galata-dergisi-workspace.
+ *
+ * galata-dergisi-workspace is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * galata-dergisi-workspace is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with galata-dergisi-workspace. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
